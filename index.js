e0cd840f1d4893ed64c4c7b0e4287e1ddcd93978
Toaster1123 <prokhorov.rostislav@gmail.com>
2024-11-30 02:11:50 +0300

    make a bot sending a table data

--- index.js
+++ index.js
@@ -1,84 +1,85 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
 const TelegramApi = require('node-telegram-bot-api');
+const { year, dayOfWeek, getData, currentDay } = require('./options');
 
 const token = '7755027930:AAG4HnATGD7KIKc5U59581gGpT5-KjaLE9g';
 
 const bot = new TelegramApi(token, { polling: true });
 
-const getData = async () => {
-  try {
-    const { data } = await axios.get(
-      'https://xn----3-iddzneycrmpn.xn--p1ai/lesson_table_show/?day=5',
-    );
-    return cheerio.load(data);
-  } catch (error) {
-    console.error(error);
-  }
+let yearCount = 1;
+
+let groupName = '';
+
+let groupOptions = {
+  reply_markup: {},
 };
 
-const setDataByGroup = async (group, chatId, $) => {
+const chooseGroup = async (paramGroup, $) => {
+  let count = 0;
   for (let i = 43; i >= 17; i--) {
-    if ($(`#g${i}`).text() == group) {
-      const groupId = '#g' + i;
-      bot.sendMessage(chatId, `Расписание группы ${group} на пятницу: `);
-      let message = '';
-      for (let i = 1; i < 9; i++) {
-        const tableData = $(groupId + '_' + i)
-          .text()
-          .trim();
-        if (tableData !== '') {
-          message += tableData + '\n' + '\n';
-        }
+    const groupName = $(`#g${i} > b`).text();
+    const yearByGroupName = Number(groupName.substring(groupName.length - 5, groupName.length - 4));
+
+    if (groupName != '' && yearByGroupName == paramGroup) {
+      groupOptions.reply_markup.inline_keyboard = groupOptions.reply_markup.inline_keyboard || [];
+      const index = Math.floor(count / 3);
+      if (count % 3 === 0) {
+        groupOptions.reply_markup.inline_keyboard.push([
+          { text: groupName, callback_data: groupName },
+        ]);
+      } else {
+        groupOptions.reply_markup.inline_keyboard[index].push({
+          text: groupName,
+          callback_data: groupName,
+        });
       }
-      return bot.sendMessage(chatId, message);
+      count++;
     }
+  }
+};
+const setDataByGroup = async (group, chatId, $) => {
+  if (group != '') {
+    for (let i = 43; i >= 17; i--) {
+      if ($(`#g${i}`).text() == group) {
+        const groupId = '#g' + i;
+        await bot.sendMessage(
+          chatId,
+          `Расписание группы ${group} на ${dayOfWeek[currentDay - 1]}: `,
+        );
+        let message = '';
+        for (let i = 1; i < 9; i++) {
+          const tableData = $(groupId + '_' + i)
+            .text()
+            .trim();
+          if (tableData !== '') {
+            message += tableData + '\n' + '\n';
+          }
+        }
+        return bot.sendMessage(chatId, message);
+      }
+    }
+  } else {
+    bot.sendMessage(chatId, 'Вы ещё не выбрали группу');
   }
 };
 
 const start = () => {
   bot.setMyCommands([
-    { command: '/start', description: 'Запустить бота' },
     { command: '/data_send', description: 'Получить данные о расписании' },
-    // { command: '/change_group', description: 'Поченять группу' },
+    { command: '/change_group', description: 'Поменять группу' },
   ]);
   bot.on('message', async (msg) => {
     const $ = await getData();
-
     const text = msg.text;
-
     const chatId = msg.chat.id;
 
-    const groupOptions = {
-      reply_markup: {},
-    };
-
-    let count = 0;
-    for (let i = 43; i >= 17; i--) {
-      const groupName = $(`#g${i} > b`).text();
-
-      if (groupName != '') {
-        groupOptions.reply_markup.inline_keyboard = groupOptions.reply_markup.inline_keyboard || [];
-        const index = Math.floor(count / 3);
-        if (count % 3 === 0) {
-          groupOptions.reply_markup.inline_keyboard.push([
-            { text: groupName, callback_data: groupName },
-          ]);
-        } else {
-          groupOptions.reply_markup.inline_keyboard[index].push({
-            text: groupName,
-            callback_data: groupName,
-          });
-        }
-        count++;
-      }
-    }
-
     if (text === '/start') {
-      return bot.sendMessage(chatId, 'Привет! Выбери свою группу:', groupOptions);
+      return bot.sendMessage(chatId, 'Привет! Выбери свой курс:', year);
     }
     if (text === '/data_send') {
-      // setDataByGroup(data, chatId, $);
+      return setDataByGroup(groupName, chatId, $);
+    }
+    if (text === '/change_group') {
+      return bot.sendMessage(chatId, 'Выбери свой курс:', year);
     }
     return bot.sendMessage(chatId, 'Извините, я не понимаю такие слова');
   });
@@ -87,9 +88,20 @@ const start = () => {
 
     const chatId = msg.message.chat.id;
 
-    const data = msg.data;
-
-    await setDataByGroup(data, chatId, $);
+    const text = msg.message.text;
+    if (text.includes('Выбери свой курс:')) {
+      yearCount = msg.data;
+      if (Object.keys(groupOptions) !== 0) {
+        groupOptions = {
+          reply_markup: {},
+        };
+      }
+      await chooseGroup(yearCount, $);
+      return await bot.sendMessage(chatId, 'Выбери свою группу:', groupOptions);
+    } else {
+      groupName = msg.data;
+      return setDataByGroup(groupName, chatId, $);
+    }
   });
 };
 start();
--- options.js
+++ options.js
@@ -1,1 +1,29 @@
-module.exports = {};
+const axios = require('axios');
+const cheerio = require('cheerio');
+const currentDay = new Date().getDay() >= 5 ? 1 : new Date().getDay();
+
+module.exports = {
+  year: {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{ text: '1 курс', callback_data: '1' }],
+        [{ text: '2 курс', callback_data: '2' }],
+        [{ text: '3 курс', callback_data: '3' }],
+        [{ text: '4 курс', callback_data: '4' }],
+      ],
+    }),
+  },
+  dayOfWeek: ['понедельник', 'вторник', 'среду', 'четверг', 'пятницу', 'субботу', 'воскресенье'],
+  getData: async () => {
+    try {
+      const { data } = await axios.get(
+        `https://xn----3-iddzneycrmpn.xn--p1ai/lesson_table_show/?day=${currentDay}`,
+      );
+      return cheerio.load(data);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  currentDay: currentDay,
+};
